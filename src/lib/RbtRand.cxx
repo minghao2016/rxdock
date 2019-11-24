55b2438cb14eb67041e97b960f843a23d1dff02b
Vedran Miletić <vedran@miletic.net>
2019-11-24 18:28:41 +0100

    Used standard C++11 RNG on Solaris and Windows/MSVC
    PCG fails to build on Solaris and Windows/MSVC. Use instead whatever the
    compiler and the standard library provide as the default C++11 RNG.
    
    Issues on GitHub:
    - Solaris: https://github.com/imneme/pcg-cpp/issues/42
    - Windows/MSVC: https://github.com/imneme/pcg-cpp/issues/11

--- src/lib/RbtRand.cxx
+++ src/lib/RbtRand.cxx
@@ -39,8 +39,13 @@ void RbtRand::Seed(int seed) { m_rng.seed(seed); }
 
 // Seed the random number generator from the random device
 void RbtRand::SeedFromRandomDevice() {
+#if !defined(__sun) && !defined(_WIN32) && !defined(_MSC_VER)
   pcg_extras::seed_seq_from<std::random_device> seed_source;
   m_rng.seed(seed_source);
+#else
+  std::random_device rd;
+  m_rng.seed(rd());
+#endif
 }
 
 // Get a random double between 0 and 1
